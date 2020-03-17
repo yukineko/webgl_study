4bea0402518ec0faaf9953518de63fdd2abb3ef0
Nakayama Hiroyuki <hiroyuki.nakayama@generosity.co.jp>
2020-03-17 18:10:25 +0900

    [modify] update 026.js

--- script026_.js
+++ script026_.js
@@ -1,109 +1,94 @@
 // sample_014
 //
 // WebGLでテクスチャを利用する
+import {create_program, create_shader, create_vbo, create_ibo, get_canvas, get_gl, get_program, set_attributes} from './glutil.js'
+
 
 onload = function(){
-	// canvasエレメントを取得
-	var c = document.getElementById('canvas');
-	c.width = 500;
-	c.height = 300;
+    const c = get_canvas('canvas', 500, 300);
+    const gl = get_gl(c)
+
+    const prg = get_program(gl, 'vs', 'fs')
 	
-	// webglコンテキストを取得
-	var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
-	
-	// 頂点シェーダとフラグメントシェーダの生成
-	var v_shader = create_shader('vs');
-	var f_shader = create_shader('fs');
-	
-	// プログラムオブジェクトの生成とリンク
-	var prg = create_program(v_shader, f_shader);
-	
-	// attributeLocationを配列に取得
-	var attLocation = new Array();
-	attLocation[0] = gl.getAttribLocation(prg, 'position');
-	attLocation[1] = gl.getAttribLocation(prg, 'color');
-	attLocation[2] = gl.getAttribLocation(prg, 'textureCoord');
-	
-	// attributeの要素数を配列に格納
-	var attStride = new Array();
-	attStride[0] = 3;
-	attStride[1] = 4;
-	attStride[2] = 2;
-	
-	// 頂点の位置
-	var position = [
-		-1.0,  1.0,  0.0,
-		 1.0,  1.0,  0.0,
-		-1.0, -1.0,  0.0,
-		 1.0, -1.0,  0.0
-	];
-	
-	// 頂点色
-	var color = [
-		1.0, 1.0, 1.0, 1.0,
-		1.0, 1.0, 1.0, 1.0,
-		1.0, 1.0, 1.0, 1.0,
-		1.0, 1.0, 1.0, 1.0
-	];
-	
-	// テクスチャ座標
-	var textureCoord = [
-		0.0, 0.0,
-		1.0, 0.0,
-		0.0, 1.0,
-		1.0, 1.0
-	];
-	
-	// 頂点インデックス
-	var index = [
-		0, 1, 2,
-		3, 2, 1
-	];
+    var attLocation = new Array();
+    attLocation[0] = gl.getAttribLocation(prg, 'position');
+    attLocation[1] = gl.getAttribLocation(prg, 'color');
+    attLocation[2] = gl.getAttribLocation(prg, 'textureCoord');
+
+    var attStride = new Array();
+    attStride[0] = 3;
+    attStride[1] = 4;
+    attStride[2] = 2;
+
+    const position = [
+        -1.0,  1.0, 0.0,
+         1.0,  1.0, 0.0,
+        -1.0, -1.0, 0.0,
+         1.0, -1.0, 0.0
+    ]
+    const color = [
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0
+    ]
+
+    const textureCoord = [
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 1.0
+    ]
+
+    const index = [
+        0, 1, 2,
+        3, 2, 1
+    ]
 	
 	// VBOとIBOの生成
-	var vPosition     = create_vbo(position);
-	var vColor        = create_vbo(color);
-	var vTextureCoord = create_vbo(textureCoord);
-	var VBOList       = [vPosition, vColor, vTextureCoord];
-	var iIndex        = create_ibo(index);
-	
-	// VBOとIBOの登録
-	set_attribute(VBOList, attLocation, attStride);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iIndex);
-	
+    var vPosition     = create_vbo(gl, position);
+    var vColor        = create_vbo(gl, color);
+    var vTextureCoord = create_vbo(gl, textureCoord);
+    var vVBOList       = [vPosition, vColor, vTextureCoord];
+    var vIndex        = create_ibo(gl, index);
+
+    set_attributes(gl, vVBOList, attLocation, attStride)
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex)
+    
 	// uniformLocationを配列に取得
-	var uniLocation = new Array();
-	uniLocation[0]  = gl.getUniformLocation(prg, 'mvpMatrix');
-	uniLocation[1]  = gl.getUniformLocation(prg, 'texture');
+
+    const uniLocation = new Array()
+    uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix')
+    uniLocation[1] = gl.getUniformLocation(prg, 'texture')
+
+    gl.activeTexture(gl.TEXTURE0);    
+    var m = new matIV();
+    var mMatrix   = m.identity(m.create());
+    var vMatrix   = m.identity(m.create());
+    var pMatrix   = m.identity(m.create());
+    var tmpMatrix = m.identity(m.create());
+    var mvpMatrix = m.identity(m.create());
+    
+    // ビュー×プロジェクション座標変換行列
+    m.lookAt([0.0, 2.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+    m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
+    m.multiply(pMatrix, vMatrix, tmpMatrix);
 	
-	// 各種行列の生成と初期化
-	var m = new matIV();
-	var mMatrix   = m.identity(m.create());
-	var vMatrix   = m.identity(m.create());
-	var pMatrix   = m.identity(m.create());
-	var tmpMatrix = m.identity(m.create());
-	var mvpMatrix = m.identity(m.create());
+    // 深度テストを有効にする
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    
+    // 有効にするテクスチャユニットを指定
+
 	
-	// ビュー×プロジェクション座標変換行列
-	m.lookAt([0.0, 2.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
-	m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
-	m.multiply(pMatrix, vMatrix, tmpMatrix);
+    // テクスチャ用変数の宣言
+    let texture = null;
 	
-	// 深度テストを有効にする
-	gl.enable(gl.DEPTH_TEST);
-	gl.depthFunc(gl.LEQUAL);
+    // テクスチャを生成
+    create_texture('./texture.png');
 	
-	// 有効にするテクスチャユニットを指定
-	gl.activeTexture(gl.TEXTURE0);
-	
-	// テクスチャ用変数の宣言
-	var texture = null;
-	
-	// テクスチャを生成
-	create_texture('./texture.png');
-	
-	// カウンタの宣言
-	var count = 0;
+    // カウンタの宣言
+    let count = 0;
 	
 	// 恒常ループ
 	function loop(){
@@ -138,129 +123,6 @@ onload = function(){
 		setTimeout(loop, 1000 / 30);
 	}
     loop()
-	
-	// シェーダを生成する関数
-	function create_shader(id){
-		// シェーダを格納する変数
-		var shader;
-		
-		// HTMLからscriptタグへの参照を取得
-		var scriptElement = document.getElementById(id);
-		
-		// scriptタグが存在しない場合は抜ける
-		if(!scriptElement){return;}
-		
-		// scriptタグのtype属性をチェック
-		switch(scriptElement.type){
-			
-			// 頂点シェーダの場合
-			case 'x-shader/x-vertex':
-				shader = gl.createShader(gl.VERTEX_SHADER);
-				break;
-				
-			// フラグメントシェーダの場合
-			case 'x-shader/x-fragment':
-				shader = gl.createShader(gl.FRAGMENT_SHADER);
-				break;
-			default :
-				return;
-		}
-		
-		// 生成されたシェーダにソースを割り当てる
-		gl.shaderSource(shader, scriptElement.text);
-		
-		// シェーダをコンパイルする
-		gl.compileShader(shader);
-		
-		// シェーダが正しくコンパイルされたかチェック
-		if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-			
-			// 成功していたらシェーダを返して終了
-			return shader;
-		}else{
-			
-			// 失敗していたらエラーログをアラートする
-			alert(gl.getShaderInfoLog(shader));
-		}
-	}
-	
-	// プログラムオブジェクトを生成しシェーダをリンクする関数
-	function create_program(vs, fs){
-		// プログラムオブジェクトの生成
-		var program = gl.createProgram();
-		
-		// プログラムオブジェクトにシェーダを割り当てる
-		gl.attachShader(program, vs);
-		gl.attachShader(program, fs);
-		
-		// シェーダをリンク
-		gl.linkProgram(program);
-		
-		// シェーダのリンクが正しく行なわれたかチェック
-		if(gl.getProgramParameter(program, gl.LINK_STATUS)){
-		
-			// 成功していたらプログラムオブジェクトを有効にする
-			gl.useProgram(program);
-			
-			// プログラムオブジェクトを返して終了
-			return program;
-		}else{
-			
-			// 失敗していたらエラーログをアラートする
-			alert(gl.getProgramInfoLog(program));
-		}
-	}
-	
-	// VBOを生成する関数
-	function create_vbo(data){
-		// バッファオブジェクトの生成
-		var vbo = gl.createBuffer();
-		
-		// バッファをバインドする
-		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-		
-		// バッファにデータをセット
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-		
-		// バッファのバインドを無効化
-		gl.bindBuffer(gl.ARRAY_BUFFER, null);
-		
-		// 生成した VBO を返して終了
-		return vbo;
-	}
-	
-	// VBOをバインドし登録する関数
-	function set_attribute(vbo, attL, attS){
-		// 引数として受け取った配列を処理する
-		for(var i in vbo){
-			// バッファをバインドする
-			gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
-			
-			// attributeLocationを有効にする
-			gl.enableVertexAttribArray(attL[i]);
-			
-			// attributeLocationを通知し登録する
-			gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
-		}
-	}
-	
-	// IBOを生成する関数
-	function create_ibo(data){
-		// バッファオブジェクトの生成
-		var ibo = gl.createBuffer();
-		
-		// バッファをバインドする
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-		
-		// バッファにデータをセット
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
-		
-		// バッファのバインドを無効化
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-		
-		// 生成したIBOを返して終了
-		return ibo;
-	}
 	
 	// テクスチャを生成する関数
 	function create_texture(source){
